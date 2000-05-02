1523206c80d22eb2a5f03bff4ad433ad6c052ac4
Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
2000-05-02 08:19:16 +0000

    kpopmenu -> kpopupmenu
    svn path=/trunk/kdemultimedia/kscd/; revision=48094

--- docking.cpp
+++ docking.cpp
@@ -30,7 +30,7 @@
 #include <klocale.h>
 #include <kiconloader.h>
 #include <kglobal.h>
-#include <kpopmenu.h>
+#include <kpopupmenu.h>
 
 DockWidget::DockWidget( KSCD* parent, const char *name)
   : KDockWindow( parent, name ) 
