75b749631171966de8cc262a9164ed1dc25ec88c
Sylvain Gastellu <sylvain.gastellu@gmail.com>
2007-11-29 12:16:21 +0000

    Creation of the Tests directory
    svn path=/branches/kscd/isi-kscd/kdemultimedia/kscd/; revision=742951

---
+++ tests/hwcontroler_test.cpp
@@ -0,0 +1,59 @@
+HWControler_test :: HWControler_test ()
+{
+
+}
+
+bool HWControler_test :: all_tests()
+{
+	return (selectCd_test() && selectSpeaker_test() && eject_test() && play_test() && nextTrack_test() && prevTrack_test() && stop_test() && pause_test() && mute_test() && isCdInserted_test());
+}
+
+bool HWControler_test :: selectCd_test()
+{
+	return true;
+}
+
+bool HWControler_test :: selectSpeaker_test()
+{
+	return true;
+}
+
+bool HWControler_test :: eject_test()
+{
+	return true;
+}
+
+bool HWControler_test :: play_test()
+{
+	return true;
+}
+
+bool HWControler_test :: nextTrack_test()
+{
+	return true;
+}
+
+bool HWControler_test :: prevTrack_test()
+{
+	return true;
+}
+
+bool HWControler_test :: stop_test()
+{
+	return true;
+}
+
+bool HWControler_test :: pause_test()
+{
+	return true;
+}
+
+bool HWControler_test :: mute_test()
+{
+	return true;
+}
+
+bool HWControler_test :: isCdInserted_test()
+{
+
+}
