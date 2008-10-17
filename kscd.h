f116d6748159b9300e3cadd3e24bd019d715d3d5
Jure Repinc <jlp@holodeck1.com>
2008-10-17 00:12:56 +0000

    Make KSCD compile
    svn path=/trunk/KDE/kdemultimedia/kscd/; revision=872347

--- kscd.h
+++ kscd.h
@@ -23,8 +23,8 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef KSCD
-#define KSCD
+#ifndef KSCD_H
+#define KSCD_H
 
 // Solid implementation
 #include <qdialog.h>
