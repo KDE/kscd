f05bad2dba8d42f28edb95863a7bbd9fdc4b1142
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:34:33 +0000

    fix Messages.sh after yesterday change in extract-messages.sh
    svn path=/trunk/KDE/kdemultimedia/kscd/; revision=1166690

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
 $XGETTEXT *.cpp gui/*.cpp -o $podir/kscd.pot
 rm -f rc.cpp
